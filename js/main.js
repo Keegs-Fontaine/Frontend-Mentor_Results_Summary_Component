8213b6b5350cee9ff443d74c9f89eda741787e60
= <keegsfontaine@gmail.com>
2023-09-04 12:01:27 -0400

    started mobile summary styling and finished js dynamic color styles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 const summaryBody = document.querySelector("[data-summaryBody]");
 
-function summaryItem(category, score, icon) {
+function summaryItem(category, score, icon, color) {
+  console.log(color);
+
   const sumItem = `
-          <section class="summary-item">
+          <section style="background-color: ${
+            color.substring(0, color.length - 1) + " / 0.15)"
+          }" class="summary__item">
               <img src="${icon}" alt="icon" />
-              <h3>${category}</h3>
-              <h4><span>${score}</span>/100</h4>
+              <h3 style="color: ${color}">${category}</h3>
+              <h4 class="summary__score"><span>${score}</span>/100</h4>
           </section>
       `;
 
@@ -17,7 +21,7 @@ async function importData() {
   const data = await res.json();
 
   data.forEach((sumItem) => {
-    summaryItem(sumItem.category, sumItem.score, sumItem.icon);
+    summaryItem(sumItem.category, sumItem.score, sumItem.icon, sumItem.color);
   });
 }
 importData();
