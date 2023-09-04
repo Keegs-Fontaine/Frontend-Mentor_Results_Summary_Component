547da2022b5ae2186fa602e6b71f03c9fa0d9151
= <keegsfontaine@gmail.com>
2023-09-04 01:35:53 -0400

    Finished initial build of dynamic content (guys I built another js framework woooo)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-const sumBody = document.querySelector("[data-summaryBody]");
+const summaryBody = document.querySelector("[data-summaryBody]");
+
+function summaryItem(category, score, icon) {
+  const sumItem = `
+          <section>
+              <img src="${icon}" />
+              <h3>${category}</h3>
+              <h4><span>${score}</span>/100</h4>
+          </section>
+      `;
+
+  summaryBody.innerHTML += sumItem;
+}
+
+async function importData() {
+  const res = await fetch("../data.json");
+  const data = await res.json();
+
+  data.forEach((sumItem) => {
+    summaryItem(sumItem.category, sumItem.score, sumItem.icon);
+  });
+}
+importData();
